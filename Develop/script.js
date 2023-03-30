0aebe09b7209bc7e388a15d360d38df4601b6173
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-03-29 18:44:38 -0700

    added if statement for length

--- Develop/script.js
+++ Develop/script.js
@@ -4,20 +4,31 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var char ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./;:<>=?@[/]^"
+// var char ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./;:<>=?@[/]^"
+var char = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: "!#$%&()*+,-./;:<>=?@[/]^"
 
+}
 // Write password to the #password input
 function writePassword() {
- var length = prompt("Please enter length of password")
- var lowercase = prompt("include lowercase?")
- var uppercase = prompt("include uppercase?")
- var numeric = prompt("include numeric?")
- var specialCharacter = prompt("include special characters?")
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  // var password = choices[Math.floor(Math.random() * char.length)];
+ var length = prompt("Please enter length of password, 8-128")
+//  var lowercase = prompt("include lowercase?")
+//  var uppercase = prompt("include uppercase?")
+//  var numeric = prompt("include numeric?")
+//  var specialCharacter = prompt("include special characters?")
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+// var password = choices[Math.floor(Math.random() * char.length)];
+passwordText.value = password;
 
-  passwordText.value = password;
+
+if (length < 8 && length > 128) {
+  alert("password must contain at least 8 characters and less 129 characters");
+  return length;
+}
 
 }
 
