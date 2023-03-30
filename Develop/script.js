e7a8dc822e95438592e4529d37251b0e43dd464e
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-03-29 17:19:48 -0700

    added prompts for lowercase,uppercase,length, numeric, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var char ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./;:<>=?@[/]^"
+
 // Write password to the #password input
 function writePassword() {
+ var length = prompt("Please enter length of password")
+ var lowercase = prompt("include lowercase?")
+ var uppercase = prompt("include uppercase?")
+ var numeric = prompt("include numeric?")
+ var specialCharacter = prompt("include special characters?")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  // var password = choices[Math.floor(Math.random() * char.length)];
 
   passwordText.value = password;
 
